5037a3d2260d4a549210c0f27b33c573a11052e5
yanuvelir <yanuvelir@gmail.com>
2024-04-27 13:44:53 +0300

    added Jlabels and edited add(lbl1)... to cycle for each as Array list

--- src/Window.java
+++ src/Window.java
@@ -1,19 +1,28 @@
 import javax.swing.*;
 import java.awt.FlowLayout;
+import java.util.Arrays;
 
-public class Window extends JFrame{
+public class Window extends JFrame {
 
-    JLabel lbl1;
-    JLabel lbl2;
+    JLabel lbl1, lbl2, lbl3, lbl4, lbl5, lbl6, lbl7;
 
-    public Window(){
+    public Window() {
         super("First JFrame Window");
         setLayout(new FlowLayout());
 
         lbl1 = new JLabel("This is the first label ");
-        lbl2 = new JLabel("This is the second label");
-        add(lbl1);
-        add(lbl2);
+        lbl2 = new JLabel("This is the second label ");
+        lbl3 = new JLabel("This is the third label ");
+        lbl4 = new JLabel("This is the forth label ");
+        lbl5 = new JLabel("This is the fifth label ");
+        lbl6 = new JLabel("This is the sixth label ");
+        lbl7 = new JLabel("This is the seventh label ");
+
+        // add(lbl1); add(lbl2); add(lbl3); add(lbl4); add(lbl5); add(lbl6); add(lbl7);
+
+        for (JLabel jLabel : Arrays.asList(lbl1, lbl2, lbl3, lbl4, lbl5, lbl6, lbl7)) {
+            add(jLabel);
+        }
 
     }
 }
